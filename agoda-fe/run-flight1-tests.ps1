aec922fc84680b165ad9348b230363fe5d1fff8f
thehung1406 <thehung1406@gmail.com>
2026-04-22 02:09:35 +0700

    update

--- agoda-fe/run-flight1-tests.ps1
+++
@@ -1,7 +0,0 @@
-$env:NODE_PATH = "D:\SQA\agoda-fe\node_modules"
-
-node D:\SQA\agoda-fe\node_modules\jest\bin\jest.js `
-  --config jest.config.js `
-  tests/components/Flight1 `
-  --runInBand `
-  --verbose
